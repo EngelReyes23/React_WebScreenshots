9a818a722dad098fcff11d0a19ed370348fb43fd
EngelReyes23 <engelreyes23@gmail.com>
2022-09-06 16:34:15 -0600

    jotai instalado

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import react from '@vitejs/plugin-react'
+import jotaiDebugLabel from 'jotai/babel/plugin-debug-label'
+import jotaiReactRefresh from 'jotai/babel/plugin-react-refresh'
 import { defineConfig } from 'vite'
-import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()]
+  plugins: [react({ babel: { plugins: [jotaiDebugLabel, jotaiReactRefresh] } })]
 })
